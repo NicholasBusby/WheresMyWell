ff3333b7cd25c94d6e28eda42bd75b150a7f85cf
Nicholas Busby <Nicholas.Busby@Live.com>
2017-08-28 19:07:15 -0700

    adding the maps

--- Wheres My Well/Wheres My Well/Wheres_My_Well.Android/MainActivity.cs
+++ Wheres My Well/Wheres My Well/Wheres_My_Well.Android/MainActivity.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-
 using Android.App;
 using Android.Content.PM;
 using Android.Runtime;
@@ -20,6 +19,7 @@ namespace Wheres_My_Well.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            Xamarin.FormsMaps.Init(this, bundle);
             LoadApplication(new App());
         }
     }
--- Wheres My Well/Wheres My Well/Wheres_My_Well.iOS/AppDelegate.cs
+++ Wheres My Well/Wheres My Well/Wheres_My_Well.iOS/AppDelegate.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-using Foundation;
+﻿using Foundation;
 using UIKit;
 
 namespace Wheres_My_Well.iOS
@@ -23,6 +19,7 @@ namespace Wheres_My_Well.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            Xamarin.FormsMaps.Init();
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
